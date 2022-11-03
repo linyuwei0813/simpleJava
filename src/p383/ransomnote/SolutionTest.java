5123a395585447ce92eff34402bb20f4e7de9817
Winnie Lin <46188337+I319821@users.noreply.github.com>
2022-11-03 13:56:12 +0800

    p383 Ransom Note

---
+++ src/p383/ransomnote/SolutionTest.java
@@ -0,0 +1,13 @@
+package p383.ransomnote;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest extends Solution {
+    @Test
+    public void test() {
+        Assertions.assertFalse(canConstruct("a", "b"));
+    }
+
+}
